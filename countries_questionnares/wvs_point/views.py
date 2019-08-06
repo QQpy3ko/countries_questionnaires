d8f5b3e495cd8884cdea0bc5284bc446347efeab
Aleksandr Vinogradov <QQpy3ko@gmail.com>
2019-08-06 19:15:19 +0300

    fix views

--- countries_questionnares/wvs_point/views.py
+++ countries_questionnares/wvs_point/views.py
@@ -45,42 +45,39 @@ def wvs_point_test(request):
             emancipative_sum = 0
             secular_sum = 0
 
-            ######add voice1, voice2 to loop!
-            # for question in questions_list:
-            #     if question.question_name == 'autonomy':
-            #         continue
-            #     if question.question_type == 'secular'
-            #         secular_sum += request.POST[question.question_name]
-            #     if question.question_type == 'emancipative'
-            #         emancipative_sum += request.POST[question.question_name]
+            #####add voice1, voice2 to loop!
+            for question in questions_list:
+                if question.question_name == 'autonomy':
+                    continue
+                if question.question_type == 'secular'
+                    secular_sum += request.POST[question.question_name]
+                if question.question_type == 'emancipative'
+                    emancipative_sum += request.POST[question.question_name]
 
 
-            # emancipative_sum += autonomy_sum
+            emancipative_sum += autonomy_sum
 
-            # left_padding = 65.3
-            # bottom_padding = 519.3
-            # center_l_coordinate = left_padding + emancipative_sum
-            # center_b_coordinate = bottom_padding + secular_sum
+            left_padding = 65.3
+            bottom_padding = 519.3
+            center_l_coordinate = left_padding + emancipative_sum
+            center_b_coordinate = bottom_padding + secular_sum
 
-            # point_length = 20
-            # point_height = 20
+            point_length = 20
+            point_height = 20
 
-            # wvs_map = Image.open('/mnt/ssd/ProHeGit/ctrs_quess/countries_questionnares/wvs_point/static/wvs_point/vws_map_template.png')
-            # point_on_wvs_map = Image.open('/mnt/ssd/ProHeGit/ctrs_quess/countries_questionnares/wvs_point/static/wvs_point/wvs_point_20x20.png')
+            wvs_map = Image.open('/mnt/ssd/ProHeGit/ctrs_quess/countries_questionnares/wvs_point/static/wvs_point/vws_map_template.png')
+            point_on_wvs_map = Image.open('/mnt/ssd/ProHeGit/ctrs_quess/countries_questionnares/wvs_point/static/wvs_point/wvs_point_20x20.png')
 
-            # wvs_map.paste(
-            #     point_on_wvs_map,
-            #     (
-            #     point_l_coordinate = center_l_coordinate - point_length
-            #     point_t_coordinate = center_b_coordinate + point_height
-            #     point_r_coordinate = center_l_coordinate + point_length
-            #     point_b_coordinate = center_b_coordinate - point_height
-            #     ),
-            #     mask=point_on_wvs_map)
-            # show_map = wvs_map.show()
-
-            #### temporary variable till the whole algorythm will be set up
-            show_map = 2
+            wvs_map.paste(
+                point_on_wvs_map,
+                (
+                point_l_coordinate = center_l_coordinate - point_length
+                point_t_coordinate = center_b_coordinate + point_height
+                point_r_coordinate = center_l_coordinate + point_length
+                point_b_coordinate = center_b_coordinate - point_height
+                ),
+                mask=point_on_wvs_map)
+            show_map = wvs_map.show()
 
             pass       
 
